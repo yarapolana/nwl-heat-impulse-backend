39a43b7f4ad143bdc58d96359b160a2a2465521c
Yara Polana <yara.polana@gmail.com>
2021-10-21 20:49:57 -0700

    emit message from service

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,8 +1,23 @@
+import { io } from '../app'
 import { sendMessage } from '../utils'
 
 export class CreateMessageService {
   async execute(text: string, user_id: string) {     
-    return await sendMessage({ text, user_id })
+    const message = await sendMessage({ text, user_id })
+
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url
+      }
+    }
+
+    io.emit('new_message', infoWS)
+
+    return message
   }
 }
 
