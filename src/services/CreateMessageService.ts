c2d7e8818cfd2c8e5c7868d71558b37121383341
Yara Polana <yara.polana@gmail.com>
2021-10-21 20:59:36 -0700

    cleanup

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -20,12 +20,3 @@ export class CreateMessageService {
     return message
   }
 }
-
-/**
- * Receive github code(string)
- * Retrieve access_token from github
- * Retrieve user information from github
- * Verify if user exists in our database
- * If exists generate a new token if not we save the user to database and generate a new token
- * Return token with user information
- */
