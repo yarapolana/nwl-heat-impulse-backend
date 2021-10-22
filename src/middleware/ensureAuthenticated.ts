681000de6a609b030ee40ceb9d1772518878870f
Yara Polana <yara.polana@gmail.com>
2021-10-21 20:06:20 -0700

    create auth middleware

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization
+
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: "token.invalid"
+    })
+  }
+
+  try {
+    const [_, token] = authToken.split(" ")
+    const { sub } = verify(token, process.env.JWT_SECRET) as { sub: string }
+
+    request.user_id = sub
+
+    next()
+  } catch (e) {
+    return response.status(401).json({
+      errorCode: "token.expired"
+    })
+  }
+}
