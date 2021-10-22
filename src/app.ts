29659bb81b9e89f66a2029c7cd2924d9651c8874
Yara Polana <yara.polana@gmail.com>
2021-10-21 19:08:34 -0700

    add json config to app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { routes } from './routes'
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT, () => console.log(`[${Date.now()}] 🚀 Running on port: ${process.env.PORT}`))
