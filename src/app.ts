d116fbfdd61a1dc7123ae6d0961d07c49f7391bb
Yara Polana <yara.polana@gmail.com>
2021-10-21 19:08:22 -0700

    add routes to application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import "dotenv/config"
 import express from 'express'
 
+import { routes } from './routes'
+
 const app = express()
+
+app.use(routes)
 
 app.listen(process.env.PORT, () => console.log(`[${Date.now()}] 🚀 Running on port: ${process.env.PORT}`))
